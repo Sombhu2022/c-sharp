d2c848651bc1fa63332889de0b2a98b94853c0db
Sombhu das <105740734+Sombhu2022@users.noreply.github.com>
2023-06-01 18:05:06 +0530

    Add files via upload

---
+++ EvenNumberInArray/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EvenNumberInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ FibonaciS/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FibonaciS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci Series..");
+            Console.WriteLine("enter the range of the fibonacci series:");
+           int n=Convert.ToInt32( Console.ReadLine());
+            int a=0; int b=1; int sum , i=0;
+
+            Console.WriteLine($"fibonacci serise are :{a} \n {b}");
+            while (i <n )
+            {
+                sum = a + b;
+                a = b;
+                b = sum;
+                Console.WriteLine(sum);
+                i++;
+
+            }
+
+        }
+    }
+}
